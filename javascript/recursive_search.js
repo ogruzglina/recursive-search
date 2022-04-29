0ba42b6ad54d9aee5db9a4180ea3aedd3bbe142d
Olha Gruzglina <olga.zhmura@gmail.com>
2022-04-29 16:47:25 -0400

    commit tree ef0b3153b4e76fa3c8dde14bd734209960444821

--- javascript/recursive_search.js
+++ javascript/recursive_search.js
@@ -1,16 +1,40 @@
+
 function recursiveSearch(arr, target) {
-  // type your code here
+  // function iterativeSearch(arr, target) {
+  //   for (const value of arr) {
+  //     if (value === target) {
+  //       return true;
+  //     }
+  //   }
+  //   return false;
+  // }
+
+  //check if arr is empty if yes - return failures
+  if (arr.length === 0) {
+    return false;
+  } else {
+    //if not, then check 1st arr elem with target, it equal then return true
+    if (arr[0] === target) {
+      return true;
+    } 
+    return recursiveSearch(arr.slice(1), target);
+  }
 }
 
 if (require.main === module) {
   // add your own tests in here
-  console.log("Expecting: true");
+  console.log("[1, 2, 3], 2 - Expecting: true");
   console.log("=>", recursiveSearch([1, 2, 3], 2));
 
-  console.log("");
+  console.log("---------");
 
-  console.log("Expecting: false");
+  console.log("[3, 2, 1], 4 - Expecting: false");
   console.log("=>", recursiveSearch([3, 2, 1], 4));
+
+  console.log('---------');
+
+  console.log("[], 4 - Expecting: false");
+  console.log("=>", recursiveSearch([], 4));
 }
 
 module.exports = recursiveSearch;
